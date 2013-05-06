c53defc219f0f6224db58d27aa38cf42736629ef
user001 <masakazu_honda@fexd.co.jp>
2013-05-07 03:17:16 +0900

    ajaxtest

--- app/controllers/webapphonda_controller.rb
+++ app/controllers/webapphonda_controller.rb
@@ -1,10 +1,18 @@
 class WebapphondaController < ApplicationController
   def index    
-   if params[:self_render]
-      render :text => ""
+   if params[:self] =="self"
+    session[:inline] = params[:text_id] 
+    aaa()
+       #render :text => params[:text_id]
+      #render params[:label_id] => params[:text_id] 
     else
-      #redirect_to :action => "render_text"
+     # redirect_to :action => "http://localhost:3000/webapphonda"
     end 
+  end
+  
+  def async_greeting
+    @name = params[:text_id] 
+    #render :text => "testaaa"  
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WebappHonda0417::Application.routes.draw do
-  get "webapphonda/index"
+  #get "webapphonda/index"
+  #post "/index"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,4 +59,8 @@ WebappHonda0417::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  
+  match "webapphonda" , :to => "webapphonda#index" , :via => [:get] 
+  match "webapphonda" , :to => "webapphonda#index" , :via => [:post] 
+  
 end
